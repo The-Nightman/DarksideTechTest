e97034fbee77e81419ea7b15837a9efa4001fcba
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-08-08 16:32:08 +0100

    configure database.php to use mysql as default and replaced unused template migrations with create form table migration

---
+++ darkside-technical/database/migrations/2024_08_08_150727_create_form_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('form', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('house_number');
+            $table->string('address_1');
+            $table->string('address_2');
+            $table->string('postcode');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('form');
+    }
+};
