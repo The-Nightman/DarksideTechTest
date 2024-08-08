2cf5209b181e8257545525df94c8748c5905704f
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-08-08 22:10:23 +0100

    add FormData interface type to cleanup code clutter

---
+++ darkside-technical/resources/js/types/formData.ts
@@ -0,0 +1,13 @@
+export interface FormData {
+  id: number | null;
+  name: string;
+  email: string;
+  phone: string;
+  house_number: string;
+  address_1: string;
+  address_2: string;
+  postcode: string;
+  city: string;
+  state: string;
+  country: string;
+}
